d952495187b169cb5e9b5d41157ab44c1df19754
seasons986 <annalin12345@gmail.com>
2023-12-05 20:09:34 -0500

    cleaning up

--- seeing machines/spamserver/src/ofApp.cpp
+++ seeing machines/spamserver/src/ofApp.cpp
@@ -4,21 +4,14 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    // add to ofApp::setup() to manipulate created windows
-    for(auto it=windows.begin(); it!=windows.end(); it++){
-        ofDefaultVec2 pos(150+cos(fmod(ofGetElapsedTimef(),TWO_PI)) * 100.f, 150+sin(fmod(ofGetElapsedTimef(),TWO_PI)) * 100.f);
-        (*it)->setWindowPosition(pos.x, pos.y);
-    }
-    
-    ofBackground(255);
-    ofSetCircleResolution(200);
+//    ofBackground(255);
+//    ofSetCircleResolution(200);
 
     ofSetFrameRate(8);
     
     // Set up the OSC receiver.
     recvPort = 3030;
     receiver.setup(recvPort);
-    
 }
 
 //--------------------------------------------------------------
@@ -28,12 +21,9 @@ void ofApp::update(){
       // Get the next message.
       ofxOscMessage msg;
       receiver.getNextMessage(msg);
-      cout << msg << endl;
-
       if (msg.getAddress() == "/cursor/move")
       {
-//        cursorX = msg.getArgAsInt(0);
-//      cursorY = msg.getArgAsInt(1);
+//          check if at least 25px offset from previous window
           if (abs(msg.getArgAsInt(0) - getX) > 25 ||
               abs(msg.getArgAsInt(1) - getY) > 25)
           {
@@ -41,13 +31,7 @@ void ofApp::update(){
               getY = msg.getArgAsInt(1);
               drawWindow();
           }
-      }
-      else if (msg.getAddress() == "/cursor/color")
-      {
-        // Generate a new random color.
-        cursorColor = ofColor(ofRandom(127, 255), ofRandom(127, 255), ofRandom(127, 255));
-      }
-      else
+      } else
       {
         ofLogWarning(__FUNCTION__) << "Unrecognized message " << msg.getAddress();
       }
@@ -56,25 +40,12 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-//    ofBackground(0);
-
-    // Draw a circle at the cursor position.
-//    ofSetColor(cursorColor);
-//    ofDrawCircle(cursorX, cursorY, 50);
     
-//    if (ofGetFrameNum() % 240)
-//    {
-//        ofApp::makeCall();
-//    }
 }
 
 //--------------------------------------------------------------
 void ofApp::exit(){
 
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
 }
 
 void ofApp::drawRandomInWindow(ofEventArgs & args){
@@ -85,69 +56,15 @@ void ofApp::drawRandomInWindow(ofEventArgs & args){
     ofPopStyle();
 }
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-    
-}
-
 void ofApp::drawWindow() {
     ofGLFWWindowSettings settings;
     settings.setSize(100,100);
-//    settings.setPosition(ofVec2f(ofRandom(0,400),ofRandom(0,600)));
-    settings.setPosition(ofVec2f(getX+150,getY+20));
+    settings.setPosition(ofVec2f(getX,getY));
     settings.resizable = false;
+    if ()
     windows.push_back( ofCreateWindow(settings) );
-    windows.back()->setVerticalSync(false);
+    cout << windows.size() << endl;
     
     // when this is front() rectangles stop drawing after 1st popup
     ofAddListener(windows.back()->events().draw, this, &ofApp::drawRandomInWindow);
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-
 }
